b754a10470ff7e2670ec235d3816a93eb944fcca
Kapil <58680946+kapildevadk@users.noreply.github.com>
2020-08-02 12:24:45 +0545

    to port into Qgis 3.x version

--- polygondivider_dialog.py
+++ polygondivider_dialog.py
@@ -23,13 +23,15 @@
 
 import os
 
-from PyQt4 import QtGui, uic
+from qgis.PyQt import uic
+from qgis.PyQt import QtWidgets
+from qgis.PyQt.QtWidgets import QDialog, QDialogButtonBox
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'polygondivider_dialog_base.ui'))
 
 
-class PolygonDividerDialog(QtGui.QDialog, FORM_CLASS):
+class PolygonDividerDialog(QtWidgets.QDialog, FORM_CLASS):
     def __init__(self, parent=None):
         """Constructor."""
         super(PolygonDividerDialog, self).__init__(parent)
