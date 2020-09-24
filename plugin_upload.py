d3fb4e589f978372964573931c225f0fbd132e06
Jonny Huck <jonnyhuck@gmail.com>
2020-09-24 18:01:41 +0100

    updated to QGIS 3

--- plugin_upload.py
+++ plugin_upload.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 # coding=utf-8
-"""This script uploads a plugin package on the server.
+"""This script uploads a plugin package to the plugin repository.
         Authors: A. Pasotti, V. Picavet
         git sha              : $TemplateVCSFormat
 """
 
 import sys
 import getpass
-import xmlrpclib
+import xmlrpc.client
 from optparse import OptionParser
 
+standard_library.install_aliases()
+
 # Configuration
-PROTOCOL = 'http'
+PROTOCOL = 'https'
 SERVER = 'plugins.qgis.org'
-PORT = '80'
+PORT = '443'
 ENDPOINT = '/plugins/RPC2/'
 VERBOSE = False
 
@@ -24,32 +26,33 @@ def main(parameters, arguments):
     :param parameters: Command line parameters.
     :param arguments: Command line arguments.
     """
-    address = "%s://%s:%s@%s:%s%s" % (
-        PROTOCOL,
-        parameters.username,
-        parameters.password,
-        parameters.server,
-        parameters.port,
-        ENDPOINT)
-    print "Connecting to: %s" % hide_password(address)
+    address = "{protocol}://{username}:{password}@{server}:{port}{endpoint}".format(
+        protocol=PROTOCOL,
+        username=parameters.username,
+        password=parameters.password,
+        server=parameters.server,
+        port=parameters.port,
+        endpoint=ENDPOINT)
+    print("Connecting to: %s" % hide_password(address))
 
-    server = xmlrpclib.ServerProxy(address, verbose=VERBOSE)
+    server = xmlrpc.client.ServerProxy(address, verbose=VERBOSE)
 
     try:
-        plugin_id, version_id = server.plugin.upload(
-            xmlrpclib.Binary(open(arguments[0]).read()))
-        print "Plugin ID: %s" % plugin_id
-        print "Version ID: %s" % version_id
-    except xmlrpclib.ProtocolError, err:
-        print "A protocol error occurred"
-        print "URL: %s" % hide_password(err.url, 0)
-        print "HTTP/HTTPS headers: %s" % err.headers
-        print "Error code: %d" % err.errcode
-        print "Error message: %s" % err.errmsg
-    except xmlrpclib.Fault, err:
-        print "A fault occurred"
-        print "Fault code: %d" % err.faultCode
-        print "Fault string: %s" % err.faultString
+        with open(arguments[0], 'rb') as handle:
+            plugin_id, version_id = server.plugin.upload(
+                xmlrpc.client.Binary(handle.read()))
+        print("Plugin ID: %s" % plugin_id)
+        print("Version ID: %s" % version_id)
+    except xmlrpc.client.ProtocolError as err:
+        print("A protocol error occurred")
+        print("URL: %s" % hide_password(err.url, 0))
+        print("HTTP/HTTPS headers: %s" % err.headers)
+        print("Error code: %d" % err.errcode)
+        print("Error message: %s" % err.errmsg)
+    except xmlrpc.client.Fault as err:
+        print("A fault occurred")
+        print("Fault code: %d" % err.faultCode)
+        print("Fault string: %s" % err.faultString)
 
 
 def hide_password(url, start=6):
@@ -85,7 +88,7 @@ if __name__ == "__main__":
         help="Specify server name", metavar="plugins.qgis.org")
     options, args = parser.parse_args()
     if len(args) != 1:
-        print "Please specify zip file.\n"
+        print("Please specify zip file.\n")
         parser.print_help()
         sys.exit(1)
     if not options.server:
@@ -95,8 +98,9 @@ if __name__ == "__main__":
     if not options.username:
         # interactive mode
         username = getpass.getuser()
-        print "Please enter user name [%s] :" % username,
-        res = raw_input()
+        print("Please enter user name [%s] :" % username, end=' ')
+
+        res = input()
         if res != "":
             options.username = res
         else:
